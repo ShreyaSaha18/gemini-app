0ab98e4a5f1ab89a9a923dd6b26189f3c38bd38f
Shreya Saha <shreya20211@gmail.com>
2026-05-07 22:43:54 +0530

    updated gemini model

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -19,7 +19,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-1.5-flash-latest",
   });
   
   const generationConfig = {
