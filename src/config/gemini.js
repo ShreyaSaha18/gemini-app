b9d1636b93b87c43a764031bc5ba815931fff095
Shreya Saha <shreya20211@gmail.com>
2026-05-09 17:44:10 +0530

    switched to direct gemini1.5 flash

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -2,7 +2,7 @@ const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
 async function run(prompt) {
   const response = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}`,
     {
       method: "POST",
       headers: {
